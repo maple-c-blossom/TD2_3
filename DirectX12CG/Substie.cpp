16c76cdd5a949f058d5d29f6f00d13c6b638f15e
NoseAlien <k021g1117@g.neec.ac.jp>
2022-12-17 17:20:03 +0900

    プレイヤーに必要な変数を粗方書き出した

---
+++ DirectX12CG/Substie.cpp
@@ -0,0 +1,1 @@
+#include "Substie.h"
--- DirectX12CG/Substie.h
+++ DirectX12CG/Substie.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "Object3d.h"
+class Substie :public MCB::Object3d
+{
+private:
+	std::vector<int> keyConfig{};
+	float shard = 0;
+	MCB::Vector3D  prevPos{};
+	MCB::Vector3D velocity{};
+	
+	const float maxShard = 100;
+
+};
