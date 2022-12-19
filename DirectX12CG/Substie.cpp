938a7addbe7ba8545b48c40be683c35834a02793
NoseAlien <k021g1117@g.neec.ac.jp>
2022-12-19 16:33:00 +0900

    キー入力を受け取って移動できるようにした

--- DirectX12CG/Substie.cpp
+++ DirectX12CG/Substie.cpp
@@ -1,1 +1,40 @@
 #include "Substie.h"
+
+using namespace MCB;
+
+void Substie::Initialize()
+{
+	prevPos = position;
+	velocity = Vector3D{ 0,0,0 };
+}
+
+void Substie::Update()
+{
+	velocity = position - prevPos;
+	prevPos = position;
+
+	velocity *= 0.8;
+	velocity.vec.y /= 0.8;
+	velocity.vec.y -= 0.015;
+	if (input->KeyPress(keyConfig[0]))
+	{
+		velocity.vec.z += 0.05;
+	}
+	if (input->KeyPress(keyConfig[1]))
+	{
+		velocity.vec.z -= 0.05;
+	}
+	if (input->KeyPress(keyConfig[2]))
+	{
+		velocity.vec.x += 0.05;
+	}
+	if (input->KeyPress(keyConfig[3]))
+	{
+		velocity.vec.x -= 0.05;
+	}
+
+	if (input->KeyPress(keyConfig[4]))
+	{
+		//‚Ë‚è‚¯‚µ‚ğo‚·
+	}
+}
--- DirectX12CG/Substie.h
+++ DirectX12CG/Substie.h
@@ -1,14 +1,21 @@
 #pragma once
 
 #include "Object3d.h"
+#include "Input.h"
+
 class Substie :public MCB::Object3d
 {
 private:
-	std::vector<int> keyConfig{};
+	Input* input = Input::GetInstance();
+
+	std::vector<int> keyConfig{ DIK_W,DIK_S,DIK_D,DIK_A,DIK_SPACE };
 	float shard = 0;
 	MCB::Vector3D  prevPos{};
 	MCB::Vector3D velocity{};
 	
 	const float maxShard = 100;
 
+public:
+	void Initialize();
+	void Update();
 };
