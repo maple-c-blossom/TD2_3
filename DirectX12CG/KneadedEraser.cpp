ec00d0333c763215e715d7aa4c59f687997c7a5a
NoseAlien <k021g1117@g.neec.ac.jp>
2022-12-20 16:58:57 +0900

    静的メンバ変数を初期化

--- DirectX12CG/KneadedEraser.cpp
+++ DirectX12CG/KneadedEraser.cpp
@@ -1,5 +1,8 @@
 #include "KneadedEraser.h"
 
+std::list<KneadedEraser*> KneadedEraser::setAllKneadedEraser{};
+std::list<KneadedEraser*> KneadedEraser::kneadedErasers{};
+
 void KneadedEraser::StaticUpdate()
 {
 	kneadedErasers = setAllKneadedEraser;
