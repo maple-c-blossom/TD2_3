92e8174632668c2b6b5a43e1cb29a36985e7258b
NoseAlien <k021g1117@g.neec.ac.jp>
2022-12-22 11:58:12 +0900

    ねり消しを持って回転する動きが出来た

---
+++ DirectX12CG/ADXUtility.cpp
@@ -0,0 +1,46 @@
+#include "ADXUtility.h"
+#include <math.h>
+
+using namespace MCB;
+
+
+Vector3D ADXUtility::Lerp(Vector3D nowPos, Vector3D newPos, float progress)
+{
+	Vector3D ret;
+	if (progress > 1)
+	{
+		progress = 1;
+	}
+	if (progress < 0)
+	{
+		progress = 0;
+	}
+	ret = nowPos + ((newPos - nowPos) * progress);
+	return ret;
+}
+
+//角度の差を求める　目的の角度へゆっくり向ける時に使ってみよう
+float ADXUtility::AngleDiff(float angle1, float angle2)
+{
+	angle1 = fmod(angle1, 2 * 3.1415);
+	angle2 = fmod(angle2, 2 * 3.1415);
+	float angle3 = angle2 - 2 * 3.1415;
+	float angle4 = angle2 + 2 * 3.1415;
+
+	float nearestAngleDiff = angle2 - angle1;
+	if (fabsf(nearestAngleDiff) > fabsf(angle3 - angle1))
+	{
+		nearestAngleDiff = angle3 - angle1;
+	}
+	if (fabsf(nearestAngleDiff) > fabsf(angle4 - angle1))
+	{
+		nearestAngleDiff = angle4 - angle1;
+	}
+	return nearestAngleDiff;
+}
+
+//inputStartからinputEndまでの値をoutputStartからoutputEndまでの値に変換する
+float ADXUtility::ValueMapping(float nowValue, float inputStart, float inputEnd, float outputStart, float outputEnd)
+{
+	return outputStart + (outputEnd - outputStart) * ((nowValue - inputStart) / (inputEnd - inputStart));
+}
