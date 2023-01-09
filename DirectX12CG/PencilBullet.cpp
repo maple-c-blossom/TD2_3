5d3f57da505898ac210d18ac813bf048533b56d3
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-01-09 10:04:23 +0900

    mehcanicalPencil

---
+++ DirectX12CG/PencilBullet.cpp
@@ -0,0 +1,21 @@
+#include "PencilBullet.h"
+#include "Util.h"
+void PencilBullet::Initialize(MCB::Model* model, float speed, MCB::Vector3D startPos, MCB::Vector3D targetPos)
+{
+	this->model = model;
+	this->speed = speed;
+	this->startPos = startPos;
+	this->endPos = targetPos;
+	lifeTime.Set(speed);
+	this->position = { startPos.vec.x,startPos.vec.y,startPos.vec.z };
+	Init();
+}
+
+void PencilBullet::Update()
+{
+	if (lifeTime.IsEnd()) return;
+	lifeTime.Update();
+	MCB::InQuad(startPos.vec.x, endPos.vec.x, lifeTime.GetEndTime() - 5, lifeTime.NowTime());
+	MCB::InQuad(startPos.vec.y, endPos.vec.y, lifeTime.GetEndTime() - 5, lifeTime.NowTime());
+	MCB::InQuad(startPos.vec.z, endPos.vec.z, lifeTime.GetEndTime() - 5, lifeTime.NowTime());
+}
