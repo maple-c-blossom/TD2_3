43b8d6f536639d0feb99dbe1f437a697c0abfc4c
WakanaIdeta <k021g1162@g.neec.ac.jp>
2023-02-08 10:57:41 +0900

    デバッグテキストの設定変更

--- DirectX12CG/DebugText.h
+++ DirectX12CG/DebugText.h
@@ -11,8 +11,8 @@ namespace MCB
 	{
 	private:
 		static const int maxCharCount = 0xff;//1フレ当たりの最大表示数
-		static const int fontWidth = 9;//フォントの横幅
-		static const int fontHeight = 18;//フォントの縦幅
+		static const int fontWidth = 48;//フォントの横幅
+		static const int fontHeight = 80;//フォントの縦幅
 		static const int fontLineCount = 14;//フォントの１行当たりの文字数
 		char buffer[maxCharCount]{};
 	public:
