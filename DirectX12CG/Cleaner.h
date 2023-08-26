20a8cd1b34671bdda394889e0188654d41a8b121
LE3A_02_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-08-26 15:06:13 +0900

    Cleaner調整

--- DirectX12CG/Cleaner.h
+++ DirectX12CG/Cleaner.h
@@ -5,8 +5,8 @@ class Boss;
 class Cleaner :public MCB::Object3d
 {
 	std::array<MCB::Vector3D, 6>movePoint;
-	int moveindex = 0;
-	int moveEndindex = 1;
+	int moveindex = 2;
+	int moveEndindex = 3;
 	Timer moveTime;
 public:
 	Boss* boss = nullptr;
